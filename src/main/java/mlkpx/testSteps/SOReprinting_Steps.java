8939153daed921b83a2f0b1368890868903e79df
Albert Monceda <albert.monceda@mlhuillier.com>
2024-04-12 17:33:55 +0800

    OR and SO Reprinting

--- src/main/java/mlkpx/testSteps/SOReprinting_Steps.java
+++ src/main/java/mlkpx/testSteps/SOReprinting_Steps.java
@@ -1,26 +1,40 @@
 package mlkpx.testSteps;
 
+import utilities.ExtentReport.ExtentReporter;
 import utilities.Logger.LoggingUtils;
 
 public class SOReprinting_Steps extends Base_Steps{
-    public void DS_TC_01()throws Exception{
-
-            click(sendOutPageObjects.sendOutLink(), "SendOut Page ");
-            if(isVisible(sendOutPageObjects.sendOutTransaction(), getText(sendOutPageObjects.sendOutTransaction()))){
-                LoggingUtils.info("Successfully Navigate for SendOut Domestic Page ");
-            }
-
-    }
 
     public void SOR_TC_01()throws Exception{
-
+        click(sendOutPageObjects.sendOutLink(), "SendOut Page ");
         click(soReprintingPageObjects.soReprintingLink(), "SendOut Page ");
         if(isVisible(soReprintingPageObjects.soReprintText(), getText(soReprintingPageObjects.soReprintText()))){
             LoggingUtils.info("Successfully Navigate for SendOut Reprint Page ");
         }
-
-        type(soReprintingPageObjects.kptnButtonInReprint(), "KPTN SOReprint field ", propertyReader.getproperty("kptn_SOReprint"));
+        type(soReprintingPageObjects.kptnButtonInReprint(), "KPTN SOReprint field ", propertyReader.getproperty("kptnvalidcode"));
         type(soReprintingPageObjects.reasonSOReprint(), "Reason SOReprint field ", propertyReader.getproperty("reason_SOReprint"));
         click(soReprintingPageObjects.reprintButton(), "Reprint Button");
+        if(isVisible(soReprintingPageObjects.printSendoutReceiptText(), getText(soReprintingPageObjects.printSendoutReceiptText()))){
+            ExtentReporter.logPass("SOR_TC_01", "Successfully Validate SO Reprinting");
+        }else{
+            ExtentReporter.logFail("SOR_TC_01", "Fail to Validate SO Reprinting");
+        }
     }
+
+    public void SOR_TC_02()throws Exception{
+        click(sendOutPageObjects.sendOutLink(), "SendOut Page ");
+        click(soReprintingPageObjects.soReprintingLink(), "SendOut Page ");
+        if(isVisible(soReprintingPageObjects.soReprintText(), getText(soReprintingPageObjects.soReprintText()))){
+            LoggingUtils.info("Successfully Navigate for SendOut Reprint Page ");
+        }
+        type(soReprintingPageObjects.kptnButtonInReprint(), "KPTN SOReprint field ", propertyReader.getproperty("invalidkptnfield"));
+        type(soReprintingPageObjects.reasonSOReprint(), "Reason SOReprint field ", propertyReader.getproperty("reason_SOReprint"));
+        click(soReprintingPageObjects.reprintButton(), "Reprint Button");
+        if(isVisible(soReprintingPageObjects.noTransactionFoundText(), getText(soReprintingPageObjects.noTransactionFoundText()))){
+            ExtentReporter.logPass("SOR_TC_02", "Successfully Validate SO Reprinting with Non-Existing KPTN");
+        }else{
+            ExtentReporter.logFail("SOR_TC_02", "Fail to Validate SO Reprinting with Non-Existing KPTN");
+        }
+    }
+
 }
--- src/test/java/kpx/actual/ORReprinting_Tests.java
+++ src/test/java/kpx/actual/ORReprinting_Tests.java
@@ -4,7 +4,8 @@ import kpx.base.BaseTest;
 import org.testng.annotations.Test;
 import utilities.ExtentReport.ExtentReporter;
 
-public class SOReprinting_Tests extends BaseTest {
+public class ORReprinting_Tests extends BaseTest {
+
     @Test(description = "Logging in via gmail and kpx")
     public void loginGoogle() throws Exception {
         this.loginSteps.signInGoogle("teller");
@@ -14,18 +15,18 @@ public class SOReprinting_Tests extends BaseTest {
         }
     }
 
-    @Test(dependsOnMethods = "loginGoogle" , priority = 1, description = "Validate SO Reprinting")
-    public void navigatingSOReprintingPage()throws Exception{
+    @Test(dependsOnMethods = "loginGoogle" , priority = 1, description = "Validate OR Reprinting")
+    public void navigatingORReprintingPage()throws Exception{
         if(this.homeSteps.isInHomePage()) {
-            this.soReprintingSteps.SOR_TC_01();
+            this.orReprintingSteps.OR_TC_01();
 
         }
 
     }
-    @Test(dependsOnMethods = "loginGoogle" , priority = 2, description = " Validate SO Reprinting with Non-Existing KPTN")
-    public void nonExistingKPTNSOR_TC_02()throws Exception{
+    @Test(dependsOnMethods = "loginGoogle" , priority = 2, description = " Validate OR Reprinting with Non-Existing KPTN")
+    public void nonExistingKPTNOR_TC_02()throws Exception{
         if(this.homeSteps.isInHomePage()) {
-            this.soReprintingSteps.SOR_TC_02();
+            this.orReprintingSteps.OR_TC_02();
 
         }
 
