8f9306e913620f21c54f40dab0f4a7491fad5a26
FrancisMercado <mlqaautomation@mlhuillier.com>
2024-04-23 19:38:17 +0800

    ShopSafe KPX

--- src/main/java/mlkpx/testSteps/WS_ShopSafe_Steps.java
+++ src/main/java/mlkpx/testSteps/WS_ShopSafe_Steps.java
@@ -23,17 +23,88 @@ public class WS_ShopSafe_Steps extends Base_Steps{
         click(wsShopSafePageObjects.walletServicesLink(), "Wallet Services Page ");
         click(wsShopSafePageObjects.shopsafeTextheader(), "Shop Safe Header");
         type(wsShopSafePageObjects.referenceNoTxtbox(), "Reference Number Field","" );
-
-        if(isVisible(wsShopSafePageObjects.shopsafeText(), getText(wsShopSafePageObjects.shopsafeText()))){
-            ExtentReporter.logPass("Shop Safe Page navigation", "Successfully Validated Shop Safe Page");
+        waitSleep(3000);
+        if(isVisible(wsShopSafePageObjects.shopsafeTextNextheader(), "")){
+            isVisible(wsShopSafePageObjects.TransactInfoText(),"");
+            isVisible(wsShopSafePageObjects.OriginText(),"");
+            isVisible(wsShopSafePageObjects.DestinationText(),"");
+            isVisible(wsShopSafePageObjects.SellerText(),"");
+            isVisible(wsShopSafePageObjects.BuyerText(),"");
+            isVisible(wsShopSafePageObjects.ItemDetailsText(),"");
+            scrollToElement(wsShopSafePageObjects.PhotosText());
+            isVisible(wsShopSafePageObjects.PhotosText(),"");
+            scrollToElement(wsShopSafePageObjects.SellerPhotosText());
+            isVisible(wsShopSafePageObjects.SellerPhotosText(),"");
+            scrollToElement(wsShopSafePageObjects.BuyerPhotosText());
+            isVisible(wsShopSafePageObjects.BuyerPhotosText(),"");
+            scrollToElement(wsShopSafePageObjects.TransactionDetailsText());
+            isVisible(wsShopSafePageObjects.TransactionDetailsText(),"");
+            isVisible(wsShopSafePageObjects.BuyerDetailsText(),"");
+            isVisible(wsShopSafePageObjects.SellerDetailsText(),"");
+            isVisible(wsShopSafePageObjects.AmountInformationText(),"");
+            scrollToElement(wsShopSafePageObjects.TotalAmountText());
+            isVisible(wsShopSafePageObjects.TotalAmountText(),"");
+            scrollToElement(wsShopSafePageObjects.ParcelStatusText());
+            isVisible(wsShopSafePageObjects.ParcelStatusText(),"");
+            scrollToElement(wsShopSafePageObjects.TrackingNoTextbox());
+            isVisible(wsShopSafePageObjects.TrackingNoTextbox(),"");
+            scrollToElement(wsShopSafePageObjects.RemarksTxtbox());
+            isVisible(wsShopSafePageObjects.RemarksTxtbox(),"");
+            scrollToElement(wsShopSafePageObjects.BackBtn());
+            isVisible(wsShopSafePageObjects.BackBtn(),"");
+            scrollToElement(wsShopSafePageObjects.UpdateStatusBtn());
+            isVisible(wsShopSafePageObjects.UpdateStatusBtn(),"");
+            scrollToElement(wsShopSafePageObjects.PrintReceiptTxtBtn());
+            isVisible(wsShopSafePageObjects.PrintReceiptTxtBtn(),"");
+            ExtentReporter.logPass("Shop Safe Page navigation", "Successfully Validate Valid Reference Number");
         }else{
-            ExtentReporter.logFail("Shop Safe Page navigation", "Failed to Validate Shop Safe Page");
+            ExtentReporter.logFail("Shop Safe Page navigation", "Failed to Validate Valid Reference Number");
             Assert.fail("Failed to Validate Shop Safe Page");
         }
     }
 
+    public void To_Validate_Transaction_Information_WS_SS_03()throws Exception{
+        click(wsShopSafePageObjects.walletServicesLink(), "Wallet Services Page ");
+        click(wsShopSafePageObjects.shopsafeTextheader(), "Shop Safe Header");
+        type(wsShopSafePageObjects.referenceNoTxtbox(), "Reference Number Field","" );
+        waitSleep(3000);
+        if(isVisible(wsShopSafePageObjects.shopsafeTextNextheader(), "")){
+            isVisible(wsShopSafePageObjects.OriginBranchVal(),"");
+            isVisible(wsShopSafePageObjects.OperatorOriginVal(),"");
+            isVisible(wsShopSafePageObjects.DateTimeOriginVal(),"");
+            isVisible(wsShopSafePageObjects.DestinationBranchVal(),"");
+            isVisible(wsShopSafePageObjects.OperatorDestinationVal(),"");
+            isVisible(wsShopSafePageObjects.DateTimeDestinationVal(),"");
+            isVisible(wsShopSafePageObjects.SellerNameVal(),"");
+            isVisible(wsShopSafePageObjects.SellerContactNoVal(),"");
+            isVisible(wsShopSafePageObjects.BuyerNameVal(),"");
+            isVisible(wsShopSafePageObjects.BuyerContactNoVal(),"");
+            isVisible(wsShopSafePageObjects.ItemNameVal(),"");
+            isVisible(wsShopSafePageObjects.ItemDescriptionVal(),"");
+            isVisible(wsShopSafePageObjects.BrandVal(),"");
+            isVisible(wsShopSafePageObjects.ModelNoVal(),"");
+            isVisible(wsShopSafePageObjects.CategoryVal(),"");
+            isVisible(wsShopSafePageObjects.WeightVal(),"");
+            isVisible(wsShopSafePageObjects.NoItemsVal(),"");
+            ExtentReporter.logPass("Shop Safe Page navigation", "Successfully Validate Transaction Information");
+        }else{
+            ExtentReporter.logFail("Shop Safe Page navigation", "Failed to Validate Transaction Information");
+            Assert.fail("Failed to Validate Shop Safe Page");
+        }
+    }
 
+    public void To_Validate_Transaction_Information_WS_SS_04()throws Exception{
+        click(wsShopSafePageObjects.walletServicesLink(), "Wallet Services Page ");
+        click(wsShopSafePageObjects.shopsafeTextheader(), "Shop Safe Header");
+        type(wsShopSafePageObjects.referenceNoTxtbox(), "Reference Number Field","" );
+        waitSleep(3000);
+        if(isVisible(wsShopSafePageObjects.shopsafeTextNextheader(), "")){
 
-
+            ExtentReporter.logPass("Shop Safe Page navigation", "Successfully Validate Transaction Information");
+        }else{
+            ExtentReporter.logFail("Shop Safe Page navigation", "Failed to Validate Transaction Information");
+            Assert.fail("Failed to Validate Shop Safe Page");
+        }
+    }
 
 }
