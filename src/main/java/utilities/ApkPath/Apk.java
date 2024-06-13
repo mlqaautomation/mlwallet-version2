416e2790e948d5c42825a4b19e79f76811058d32
mr-kaiden <john.campanilla@mlhuillier.com>
2024-06-13 08:51:57 +0800

    code modification

--- src/main/java/utilities/ApkPath/Apk.java
+++ src/main/java/utilities/ApkPath/Apk.java
@@ -7,7 +7,7 @@ public class Apk {
     public static final Path API_DEMOS_APK;
 
     static {
-        String filePath = "src/main/resources/mlwallet.apk";
+        String filePath = "src/main/resources/mlwallet-app.apk";
         API_DEMOS_APK = Paths.get(filePath).toAbsolutePath().normalize();
     }
 
